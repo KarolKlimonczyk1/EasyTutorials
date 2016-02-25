6bc05a9d7b364b38cc28954a28811200bb94b03d
Karol Klimończyk <mlody1q@tlen.pl>
2016-02-25 21:02:25 +0100

    Basic working version

---
+++ rest.resources/com/rest/controllers/CountryController.java
@@ -0,0 +1,29 @@
+package com.rest.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+//@RestController
+public class CountryController {
+
+	
+	
+	
+	
+//	
+//	@RequestMapping(value = "/", method = RequestMethod.GET,headers="Accept=application/json")  
+//	 public String getCountries()  
+//	 {  
+//			return "what a pitty"; 
+//	 }  
+//	
+	
+	
+	  
+
+	  
+	
+
+}
---
+++ rest.resources/com/rest/controllers/PrimaryController.java
@@ -0,0 +1,16 @@
+package com.rest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PrimaryController {
+
+//	@RequestMapping(value = { "" }, method = RequestMethod.GET)
+//	public String homePage(ModelMap model) {
+//		
+//		return "index";
+//	}
+}
